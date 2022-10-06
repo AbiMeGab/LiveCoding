4516ee46a48efd2f8f535b0e1a19dadc148dec94
AbiMeGab <abbybaem@gmail.com>
2022-10-06 18:20:03 -0500

    Live coding 5

---
+++ codigo-otros-5/src/Codigo5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Codigo5 {
+	public static void main(String[] args) {
+
+	    Scanner s = new Scanner(System.in);
+	    System.out.print("Introduzca un número del 1 al 10 y descubra si la fortuna está de su lado: ");
+	    
+	    int c = s.nextInt();
+	    int ni = c;
+	    
+	    int afo = 0;
+	    int noAfo = 0;
+	    
+	    while (ni > 0) {
+		  int digito = (int)(ni % 10);
+	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
+			afo++;
+	      } else {
+			noAfo++;
+	    }
+	      ni-=digito;
+	      ni/=10;
+	    }
+	    if (afo > noAfo) {
+	      System.out.println("El " + c + " es un número afortunado.");
+	    } else {
+	      System.out.println("El " + c + " no es un número afortunado.");
+	    }
+	    
+	  }
+	  
+	}
